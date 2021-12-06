44c4fc80d2a0ff77587872620eb5351071a17b6d
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-05 21:20:02 -0700

    added initial files

---
+++ server.js
@@ -0,0 +1,23 @@
+// requires express 
+const express = require('express');
+// const { notes } = require('/data/notes.json');
+
+const PORT = process.env.PORT || 3009;
+// instantiate the server
+const app = express();
+
+
+// express middleware 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+app.get('/api/notes', (req, res) => {
+    res.send('Hello!');
+  });
+
+app.listen(3009, () => {
+    console.log(`API server now on port 3009!`);
+  });
