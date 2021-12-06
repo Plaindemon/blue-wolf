0cc996c79e27cc64dcc5037a18162a17c06fe845
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-05 22:28:37 -0700

    Add heroku

--- server.js
+++ server.js
@@ -10,12 +10,29 @@ const app = express();
 // express middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-
+function filterByQuery(query, notesArray) {
+  let filterResults = notesArray;
+  if (query.date) {
+    filterResults = filterResults.filter(notes => notes.date === query.date);
+  }
+  if (query.id) {
+    filterResults = filterResults.filter(notes => notes.id === query.id)
+  }
+  if (query.tag) {
+    filterResults = filterResults.filter(notes => notes.tag === query.tag)
+  }
+  return filterResults;
+}
 
 app.get('/api/notes', (req, res) => {
-    res.send('Hello!');
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+    res.json(results);
+    // res.send('Hello!');
   });
 
 app.listen(3009, () => {
