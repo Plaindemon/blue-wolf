e5507fa6ae77936b33f3837ffafcdaa0626e9bff
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-20 13:19:31 -0700

    fixed the app.post route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // requires express 
 const express = require('express');
-const notes = require('./db/db.json');
+const notes  = require('./db/db.json');
+const fs = require('fs');
+const path = require('path');
+const { filterByQuery, findById, createNewNote, validateNotes } = require('./lib/notes');
+
 
 const PORT = process.env.PORT || 3009;
 // instantiate the server
@@ -12,40 +16,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-function filterByQuery(query, notesArray) {
-  let filterResults = notesArray;
-  if (query.date) {
-    filterResults = filterResults.filter(notes => notes.date === query.date);
-  }
-  if (query.id) {
-    filterResults = filterResults.filter(notes => notes.id === query.id)
-  }
-  if (query.tag) {
-    filterResults = filterResults.filter(notes => notes.tag === query.tag)
-  }
-  return filterResults;
-}
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
-function findById(id, notesArray) {
-  const result = notesArray.filter(notes => notes.id === id)[0];
-  return result;
-}
 
 
 app.get('/api/notes', (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-    res.json(results);
-    // res.send('Hello!');
+  
+  notes = JSON.parse(fs.readFileSync('./db/db.json')) || [] 
+  res.json(notes)
+      // res.send('Hello!');
   });
 
-app.get('/api/notes/:id', (req, res) => {
-  const result = findById(req.params.id, notes);
-  res.json(result);
-})
 
+app.post('/api/notes', (req, res) => {
+  // set id based on what the next index of the array will be
+  let dataVar = { 
+    id: Map.floor(Math.random()*90000),
+    title: req.body.title,
+    text: req.body.text
+  }
+  notes.push(dataVar)
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes), function(error) {
+    if (error) throw error;
+  })
+  res.json(notes)
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
